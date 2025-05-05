4a1f55d919528761d614ce9158d5f013ab657f77
ManiacAyu <Ayush Dhoke>
2025-05-05 12:17:04 +0530

    commit-130

--- frontend-react/src/Config/apiConfig.js
+++ frontend-react/src/Config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const jwtToken = localStorage.getItem("jwt");
-export const API_BASE_URL = "http://localhost:5454";
+export const API_BASE_URL = "https://bloggingsite-34eu.onrender.com";
 export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
